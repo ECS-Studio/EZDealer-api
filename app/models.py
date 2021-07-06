e5f6d5ae0bb0491f465d5501690778ba1ea95e29
eric <erichoward2141@gmail.com>
2021-07-06 18:51:38 -0500

    hotfix for class models for inventory, dealer, customer, and users

--- app/models.py
+++ app/models.py
@@ -8,3 +8,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class User(models.Model):
+    """Database model for Users"""
+class Dealer(models.Model):
+  """Class Model for Dealer"""
+
+class Customer(models.Model):
+    """Class Model for Customer"""
+
+class Inventory(models.Model):
+    """Class model for Inventory"""
